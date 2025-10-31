08274d2a1ae214bdba2c45ae47eb247054b1198b
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-10-31 13:09:06 +0100

    chore: use uno merge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import type { ClassValue } from 'class-variance-authority/types'
 import { cx } from 'class-variance-authority'
-import { twMerge } from 'tailwind-merge'
+import { unoMerge} from 'unocss-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(cx(inputs))
+  return unoMerge(cx(inputs))
 }
